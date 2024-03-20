b05ea928ea2f7c91bd573ee2a0ee92d30d929589
Ristavor <peampate@gmail.com>
2024-03-20 22:48:40 +0300

    set NoAuth

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App
-      authorizationStatus = {AuthorizationStatus.Auth}
+      authorizationStatus = {AuthorizationStatus.NoAuth}
       offers = {offers}
     />
   </React.StrictMode>
