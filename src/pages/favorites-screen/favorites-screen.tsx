aa52a9c1baadebd0978d1ed91eefaf6eeea424da
Ristavor <peampate@gmail.com>
2024-03-20 22:12:15 +0300

    add logo route

--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -1,13 +1,21 @@
-function FavoritesScreen(): JSX.Element {
+import { AppRoute } from '../../const';
+import { Link } from 'react-router-dom';
+import { Offers } from '../../types/types';
+
+type FavoritesScreenProps = {
+  offers: Offers;
+};
+
+function FavoritesScreen({offers}: FavoritesScreenProps): JSX.Element {
   return (
     <div className="page">
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link" href="main.html">
+              <Link className="header__logo-link" to={{ pathname: AppRoute.Main }}>
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
+              </Link>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -1,5 +1,7 @@
 import { Offers } from '../../types/types';
 import PlaceCardsList from '../../components/place-card-list/place-cards-list';
+import { AppRoute } from '../../const';
+import { Link } from 'react-router-dom';
 
 type MainScreenProps = {
   offers: Offers;
@@ -12,9 +14,9 @@ function MainScreen({offers}: MainScreenProps): JSX.Element {
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
+              <Link className="header__logo-link header__logo-link--active" to={{ pathname: AppRoute.Main }}>
                 <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
+              </Link>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
