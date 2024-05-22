3036f5dadb2d60da29d21c69ab065cad16d4ea1f
Ristavor <peampate@gmail.com>
2024-05-23 01:27:30 +0300

    done x2

--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -1,13 +1,12 @@
 import { AppRoute } from '../../const';
 import { Link, NavLink } from 'react-router-dom';
-import { CardType, Offers } from '../../types/types';
+import { CardType } from '../../types/types';
 import CardListComponent from '../../components/card-list/card-list';
+import { useSelector } from 'react-redux';
+import { selectOffers } from '../../store/selectors';
 
-type FavoritesScreenProps = {
-  offers: Offers;
-};
-
-function FavoritesScreen({offers}: FavoritesScreenProps): JSX.Element {
+function FavoritesScreen(): JSX.Element {
+  const offers = useSelector(selectOffers);
   // Функция для фильтрации отелей по наличию закладок
   const filterBookmarkedOffers = (city: string) => offers.filter((offer) => offer.city.title === city && offer.isBookmarked);
 
